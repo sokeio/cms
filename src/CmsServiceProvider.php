99c8b872c8ea5c7899b7df9945056f77df1a8a83
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-01-09 01:13:36 +0700

    remove crud

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -57,10 +57,10 @@ class CmsServiceProvider extends ServiceProvider
         Platform::Ready(function () {
             if (sokeio_is_admin()) {
                 Menu::Register(function () {
-                    Menu::route(['name' => 'admin.page-list', 'params' => []], 'Pages', '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-pagekit" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M12.077 20h-5.077v-16h11v14h-5.077"></path>
-             </svg>', [], 'admin.page-list');
+            //         Menu::route(['name' => 'admin.page-list', 'params' => []], 'Pages', '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-pagekit" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            //     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            //     <path d="M12.077 20h-5.077v-16h11v14h-5.077"></path>
+            //  </svg>', [], 'admin.page-list');
                     menu::subMenu('Posts', '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-text" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
@@ -70,9 +70,9 @@ class CmsServiceProvider extends ServiceProvider
                 <path d="M9 17l6 0"></path>
              </svg>', function (MenuBuilder $menu) {
                         $menu->setTargetId('cms_post_menu');
-                        $menu->route(['name' => 'admin.post-list', 'params' => []], 'Posts', '', [], 'admin.post-list');
-                        $menu->route(['name' => 'admin.catalog-list', 'params' => []], 'Catalogs', '', [], 'admin.catalog-list');
-                        $menu->route(['name' => 'admin.tag-list', 'params' => []], 'Tags', '', [], 'admin.tag-list');
+                        // $menu->route(['name' => 'admin.post-list', 'params' => []], 'Posts', '', [], 'admin.post-list');
+                        // $menu->route(['name' => 'admin.catalog-list', 'params' => []], 'Catalogs', '', [], 'admin.catalog-list');
+                        // $menu->route(['name' => 'admin.tag-list', 'params' => []], 'Tags', '', [], 'admin.tag-list');
                     });
                     Menu::attachMenu('system_appearance_menu', function (MenuBuilder $menu) {
                         $menu->route(['name' => 'admin.menu', 'params' => []], 'Menus', '', [], 'admin.menu');
