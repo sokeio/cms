85d6fbfd478b5a4ca2e4e9dd0533fa819370bb16
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-01-28 23:29:42 +0700

    Update index.blade.php

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -1,6 +1,7 @@
 <div>
-    <div x-ref="paneladmin12345678" class="p-2 position-fixed w-100 text-bg-blue bg-blue top-0 left-0" style="z-index: 1100">
-        Test
+    <div x-ref="paneladmin12345678" class="p-2 position-fixed w-100 text-bg-blue bg-blue top-0 left-0"
+        style="z-index: 1100">
+        DEMO PANEL CMS same wordpress
     </div>
     <div x-data="{
         test() {
