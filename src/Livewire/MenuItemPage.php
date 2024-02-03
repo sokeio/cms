b02ad3a1c7d5f3803dc5a8dfc21abfcf235b3e4d
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-02-03 14:48:09 +0700

    Update MenuItemPage.php

--- src/Livewire/MenuItemPage.php
+++ src/Livewire/MenuItemPage.php
@@ -2,8 +2,23 @@
 
 namespace Sokeio\Cms\Livewire;
 
+use Sokeio\Cms\Models\Page;
 use Sokeio\Components\FormMenu;
+use Sokeio\Components\UI;
 
 class MenuItemPage extends FormMenu
 {
+    public function SearchPages($text)
+    {
+        $this->skipRender();
+        return Page::query()->where('name', 'like', '%' . $text . '%')->limit(20)->get(['id', 'name']);
+    }
+    protected function MenuUI()
+    {
+        return [
+            UI::SelectWithSearch('data')->Label(__('Page'))->required()->SearchDataSource('SearchPages')->DataSource(function () {
+                return $this->SearchPages('');
+            }),
+        ];
+    }
 }
