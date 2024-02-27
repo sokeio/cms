840b3d872724cd73fb3c2ae6c521ca31bdb3506f
nguyen van hau <nguyenvanhau.soft@gmail.com>
2024-02-27 10:07:37 +0700

    move shortcode to base

--- routes/admin/common.php
+++ routes/admin/common.php
@@ -1,8 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Sokeio\Cms\Livewire\ShortcodeSetting;
-
-Route::group(['as' => 'admin.'], function () {
-    Route::post('/setting/shortcode', ShortcodeSetting::class)->name('shortcode-setting');
-});
